9e28fd850c1f4ad457d3cc57cf240e3ff00907b4
snerva <selenenerva@live.it>
2022-10-05 17:38:26 +0200

    syntax adjustment

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 //Chiedi all’utente il suo nome
-const userFirstName = prompt('Qual\'é il tuo nome?')
+const userFirstName = prompt('Qual\'é il tuo nome?');
 console.log(userFirstName);
 //chiedi il suo cognome,
-const userLastName = prompt('Qual\'é il tuo cognome?')
+const userLastName = prompt('Qual\'é il tuo cognome?');
 //chiedi il suo colore preferito
-const userBestColor = prompt('Qual\'é il tuo colore preferito?')
+const userBestColor = prompt('Qual\'é il tuo colore preferito?');
 //scrivi sulla pagina nomecognomecolorepreferito22
-document.getElementById("your_password").innerHTML = `${userFirstName + userLastName + userBestColor}22`
+document.getElementById("your_password").innerHTML = `${userFirstName + userLastName + userBestColor}22`;
